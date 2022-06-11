4885e12b6ca4719a8dd44b6119921ddf35d80650
Matheus Gomes <math.s.gomes@gmail.com>
2022-06-11 01:11:58 -0300

    Arquivo projeto CRUD remanejado para outro repositório

--- projeto/telas/404.php
+++
@@ -1,5 +0,0 @@
-<h1>Página não encontrada</h1>
-
-<img src="../img/undraw_page_not_found.svg" width="60%">
-
-<!-- https://undraw.co/search -->
--- projeto/telas/home.php
+++
@@ -1,6 +0,0 @@
-<h1>Página inicial</h1>
-<hr>
-
-<div class="alert alert-success" role="alert">
-  Bem vindo a página inicial!
-</div>
